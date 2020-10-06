11fff5efee9e85a6ea85c7f3661c1e2c7318e998
Joshua Kelly <jdjkelly@gmail.com>
2020-10-06 17:02:30 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [incidenceRate, setIncidenceRate] = useState(1);
+  const [falsePositiveRate, setFalsePositiveRate] = useState(1);
+
+  const truePositives = 10000 * (parseFloat(incidenceRate) / 100);
+  const falsePositives = 10000 * (parseFloat(falsePositiveRate) / 100);
+  const totalPositives = truePositives + falsePositives;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <p> With the following rates: </p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          True incidence rate in population (%) {incidenceRate} <br/>
+          <input type="number" min="0" max="100" step="0.1" value={incidenceRate} onChange={(event) => setIncidenceRate(event.target.value)} ></input>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          False positive rate for testing (%) {falsePositiveRate} <br/>
+          <input type="number" min="0" max="100" step="0.1" value={falsePositiveRate} onChange={(event) => setFalsePositiveRate(event.target.value)}></input>
+        </p>
+
+      <p>In a test of 10,000 people you could expect:</p>
+      <ul>
+        <li>True positives {Math.floor(truePositives)}</li>
+        <li>False Positives {Math.floor(falsePositives)}</li>
+        <li>Total positives {Math.floor(totalPositives)}</li>
+        <li>False positives being {Math.floor(falsePositives / totalPositives * 100)}% of all positives</li>
+      </ul>
     </div>
   );
 }
