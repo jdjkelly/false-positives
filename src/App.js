2e2cd909cb055b4609426eabd0dd97c40c00c70e
Joshua Kelly <jdjkelly@gmail.com>
2020-10-06 17:23:58 -0400

    rate out of 1000

--- src/App.js
+++ src/App.js
@@ -4,24 +4,28 @@ import './App.css';
 function App() {
   const [incidenceRate, setIncidenceRate] = useState(1);
   const [falsePositiveRate, setFalsePositiveRate] = useState(1);
+  const [population, setPopulation] = useState(10000);
 
-  const truePositives = 10000 * (parseFloat(incidenceRate) / 100);
-  const falsePositives = 10000 * (parseFloat(falsePositiveRate) / 100);
+  const truePositives = population * (parseInt(incidenceRate) / 1000);
+  const falsePositives = population * (parseFloat(falsePositiveRate) / 100);
   const totalPositives = truePositives + falsePositives;
 
   return (
     <div className="App">
       <p> With the following rates: </p>
-        <p>
-          True incidence rate in population (%) {incidenceRate} <br/>
-          <input type="number" min="0" max="100" step="0.1" value={incidenceRate} onChange={(event) => setIncidenceRate(event.target.value)} ></input>
-        </p>
-        <p>
-          False positive rate for testing (%) {falsePositiveRate} <br/>
-          <input type="number" min="0" max="100" step="0.1" value={falsePositiveRate} onChange={(event) => setFalsePositiveRate(event.target.value)}></input>
-        </p>
-
-      <p>In a test of 10,000 people you could expect:</p>
+      <p>
+        True incidence rate in population ({incidenceRate} out of 1000) <br/>
+        <input type="number" min="0" max="100" step="1" value={incidenceRate} onChange={(event) => setIncidenceRate(event.target.value)} ></input>
+      </p>
+      <p>
+        False positive rate for testing ({falsePositiveRate}%)<br/>
+        <input type="number" min="0" max="100" step="0.1" value={falsePositiveRate} onChange={(event) => setFalsePositiveRate(event.target.value)}></input>
+      </p>
+      <p> And a test of this many people: <br/>
+      <input type="number" min="0" max="10000000" value={population} onChange={(event) => setPopulation(event.target.value)}></input>
+      </p>
+    
+      <p>You could expect:</p>
       <ul>
         <li>True positives {Math.floor(truePositives)}</li>
         <li>False Positives {Math.floor(falsePositives)}</li>
